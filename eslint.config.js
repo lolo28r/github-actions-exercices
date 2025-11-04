80cb174f4781d7aae395f59910e62e0ddf309809
lolo28r <166133068+lolo28r@users.noreply.github.com>
2025-11-04 14:22:45 +0100

    Delete eslint.config.js

--- eslint.config.js
+++
@@ -1,9 +0,0 @@
-import { FlatCompat } from "eslint-define-config";
-
-const compat = new FlatCompat({
-    baseDirectory: process.cwd(),
-});
-
-export default [
-    ...compat.extends("eslint:recommended"),
-];
